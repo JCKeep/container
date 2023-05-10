17897c2800e93dd34dcaff74e72c07c5127aa7fc
JCKeep <2407018371@qq.com>
2023-05-10 21:28:39 +0800

    action 3

--- libs/container-images/src/image_pack.rs
+++ libs/container-images/src/image_pack.rs
@@ -7,15 +7,13 @@ use std::{
 
 use flate2::{write::GzEncoder, Compression};
 
-use crate::{binding::IMAGES_DIR, r_str};
-
 /// add the image to a tar file
 pub fn pack_image(name: &str) -> io::Result<()> {
     let filep = format!("{}{}", name, ".tar");
 
     use std::process::Command;
     let mut child = Command::new("tar")
-        .current_dir(r_str!(IMAGES_DIR))
+        .current_dir("/root/D/kernel/demo-container/images")
         .arg("-cvf")
         .arg(filep)
         .arg(name)
@@ -27,7 +25,7 @@ pub fn pack_image(name: &str) -> io::Result<()> {
 
 /// use gzip compress the image
 pub fn gz_compress_image(name: &str) -> io::Result<()> {
-    let path = format!("{}/{}", r_str!(IMAGES_DIR), name);
+    let path = format!("/root/D/kernel/demo-container/images/{}", name);
     let file = File::open(format!("{}{}", path, ".tar"))?;
 
     let image_path = format!("{}{}", path, ".tar.gz");
